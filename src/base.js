50a60afe16bca1f55876ea85495963cc95a604b1
Sade <smithsade13@gmail.com>
2019-01-18 15:20:19 -0600

    add conig file

--- src/base.js
+++ src/base.js
@@ -1,11 +1,6 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
-
-var config = {
-    apiKey: "AIzaSyAQe1-ntlthGGr_QFkGa16YPXA6COra-1o",
-    authDomain: "catch-of-the-day-f8861.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-f8861.firebaseio.com"
-};
+import config from './config.js';
 
 const firebaseApp = firebase.initializeApp(config);
 const base = Rebase.createClass(firebaseApp.database());
---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    apiKey: "AIzaSyAQe1-ntlthGGr_QFkGa16YPXA6COra-1o",
+    authDomain: "catch-of-the-day-f8861.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-f8861.firebaseio.com"
+};
+
+export default config;
